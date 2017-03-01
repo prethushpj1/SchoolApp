c8e41670cf17876b48c1c90c2d5568a1f2b0b932
Prethush <prethush.pj@experionglobal.com>
2017-03-01 23:35:03 +0530

    attendance report, childres and childres list UI

--- SchoolApp/Controllers/AttendanceReportController.swift
+++ SchoolApp/Controllers/AttendanceReportController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class AttendanceReportController: BaseController {
     @IBOutlet weak var vwOutlineBox: UIView!
+    @IBOutlet weak var tblReport: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +18,9 @@ class AttendanceReportController: BaseController {
         // Do any additional setup after loading the view.
         vwOutlineBox.layer.backgroundColor = UIColor(red: 255/255.0, green: 201/255.0, blue: 14/255.0, alpha: 1).cgColor
         vwOutlineBox.layer.masksToBounds = true
+        
+        tblReport.delegate = self
+        tblReport.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,4 +28,25 @@ class AttendanceReportController: BaseController {
         // Dispose of any resources that can be recreated.
     }
 
+}
+
+extension AttendanceReportController: UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 30.0
+    }
+}
+
+extension AttendanceReportController: UITableViewDataSource{
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let attendanceCell = tableView.dequeueReusableCell(withIdentifier: "attendaceReport", for: indexPath) as! AttendanceReportCell
+        
+        return attendanceCell
+    }
 }
---
+++ SchoolApp/ListCells/AttendanceReportCell.swift
@@ -0,0 +1,26 @@
+//
+//  AttendanceReportCell.swift
+//  SchoolApp
+//
+//  Created by Prethush on 01/03/17.
+//  Copyright © 2017 AITS Inc. All rights reserved.
+//
+
+import UIKit
+
+class AttendanceReportCell: UITableViewCell {
+    @IBOutlet weak var lblDate: UILabel!
+    @IBOutlet weak var lblStatus: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
